230a696adb79bb66c9c369a0eb7f1dca72fbf63b
Erin Walker <erinb0317@hotmail.com>
2018-03-12 20:38:00 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -37,7 +37,7 @@ class Dog
 
   def self.create(name:, breed:)
     dog = Dog.name(name, breed)
-    dog.save 
+    dog.save
     dog  
   end
 
@@ -47,6 +47,19 @@ class Dog
     new_dog.name = row[1]
     new_dog.breed = row[2]
     new_dog
+  end
+
+  def self.find_by_id(id)
+    sql = <<-SQL
+      SELECT *
+      FROM dogs
+      WHERE id = ?
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql, id).map do |row|
+      self.new_from_db(row)
+    end.first
   end
 
   def self.find_by_name(name)
