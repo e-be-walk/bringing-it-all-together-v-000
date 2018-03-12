2dd482177f2f09921bb90e1b0e8d40e872602f69
Erin Walker <erinb0317@hotmail.com>
2018-03-12 20:52:42 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -58,12 +58,11 @@ class Dog
       SELECT *
       FROM dogs
       WHERE id = ?
-      LIMIT 1
     SQL
 
     DB[:conn].execute(sql, id).map do |row|
       self.new_from_db(row)
-    end.first
+    end
   end
 
   def self.find_or_create_by(name:, breed:)
